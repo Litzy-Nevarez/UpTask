3e90df42fa3e631debc1a898fce4f1d789d1f52a
Litzy Yulissa Nevarez Garcia <19040372@itdurango.edu.mx>
2024-04-09 10:02:50 -0600

    instalacion de dependencias

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'juandelatorre/up-task_mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '42f5429914823d218c9da4c7d179234100313f47',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'juandelatorre/up-task_mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '42f5429914823d218c9da4c7d179234100313f47',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
